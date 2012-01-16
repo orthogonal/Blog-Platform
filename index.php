7394e00200046cf1c6db32e3a1254957b3aea308
Andrew Latham <acl68@case.edu>
2012-01-16 15:57:35 -0600

    Added PHP code for the index to log into the database.  Also tested it and it works.

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <? //index.php
-echo "Hello!";
+require_once("dblogin.php");
+$db_server = mysql_connect($db_hostname, $db_username, $db_password);
+mysql_select_db($db_database, $db_server);
+$query = "SELECT * FROM blog_users WHERE id = 1";
+$result = mysql_query($query) or die(mysql_error());
+
+$testtext = mysql_fetch_row($result);
+echo "$testtext[1]";
 ?>
