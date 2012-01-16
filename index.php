4f9eef5c45d7017541209881a47043f66feb1515
Andrew Latham <acl68@case.edu>
2012-01-16 17:58:56 -0600

    Added page content in HTML and CSS - an outline for a navbar and header.

--- index.php
+++ index.php
@@ -5,19 +5,46 @@ echo <<<_HDOC
 <head>
 <title>Andy's Blog</title>
 <link rel="stylesheet" type="text/css" href="http://www.lathamcity.com/blog/toadstyle.css" />
+<script src="http://lathamcity.com/_js/jquery-1.7.js"></script>
+<script>
+$('document').ready(function() {
+
+}); //end ready
+</script>
 </head>
 <body>
 _HDOC;
 require_once("dblogin.php");
 $db_server = mysql_connect($db_hostname, $db_username, $db_password);
 mysql_select_db($db_database, $db_server);
-$query = "SELECT * FROM blog_users WHERE id = 1";
-$result = mysql_query($query) or die(mysql_error());
 
-$testtext = mysql_fetch_row($result);
-echo "$testtext[1]";
-echo "<br /><br /><div id='testdiv'>Hello</div>";
+echo <<<_HDOC
+	<div id="header">
+		Andy's Blog
+	</div>
+	
+	<div id="linkbar">
+		<ul id="links">
+_HDOC;
+
+if ($_COOKIE['main'] == "")
+	echo  "<li class='menulinks'>Register</li>  <li class='menulinks'>Login</li>";
+else{
+	$cookieparts = explode('&', $_COOKIE['main']);
+	echo "<li class='menulinks'>Welcome $cookieparts[0]</li>  <li class='menulinks'>Logout</li>";
+}
+
+echo "<li class='menulinks'>About</li>  </ul>  <div class='clearfix'></div></div>";
+
+
+
+
+
 
 mysql_close($db_server);
-echo "</body></html>";
+echo <<<_HDOC
+
+</body>
+</html>
+_HDOC;
 ?>
