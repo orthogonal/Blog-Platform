9d68ff160a2f3157e65f6f964f73b91f7017b802
Andrew Latham <acl68@case.edu>
2012-01-16 16:15:28 -0600

    Sketched out an outline for the HTML document, added a CSS stylesheet.

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <? //index.php
+echo <<<_HDOC
+<!DOCTYPE html>
+<html>
+<head>
+<title>Andy's Blog</title>
+<link rel="stylesheet" type="text/css" href="http://www.lathamcity.com/blog/toadstyle.css" />
+</head>
+<body>
+_HDOC;
 require_once("dblogin.php");
 $db_server = mysql_connect($db_hostname, $db_username, $db_password);
 mysql_select_db($db_database, $db_server);
@@ -7,4 +16,8 @@ $result = mysql_query($query) or die(mysql_error());
 
 $testtext = mysql_fetch_row($result);
 echo "$testtext[1]";
+echo "<br /><br /><div id='testdiv'>Hello</div>";
+
+mysql_close($db_server);
+echo "</body></html>";
 ?>
