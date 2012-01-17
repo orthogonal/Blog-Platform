ad24bce96b732c01323a181f3f533d9b273cd594
Andrew Latham <acl68@case.edu>
2012-01-17 00:15:09 -0600

    Added HTML setup for the register/login forms.

--- header.php
+++ header.php
@@ -14,15 +14,48 @@ echo <<<_HDOC
 _HDOC;
 
 if ($_COOKIE['main'] == "")
-	echo  "<li class='menulinks'>Register</li>  <li class='menulinks'>Login</li>";
+	echo  "<li class='menulinks'><a>Register</a></li>  <li class='menulinks'><a>Login</a></li>";
 else{
 	$cookieparts = explode('&', $_COOKIE['main']);
-	echo "<li class='menulinks'>Welcome $cookieparts[0]</li>  <li class='menulinks'>Logout</li>";
+	echo "<li class='menulinks'>Welcome $cookieparts[0]</li>  <li class='menulinks' class='link'>Logout</li>";
 }
 
 echo "<li class='menulinks'>About</li>  </ul>  <div class='clearfix'></div></div>";
 
-echo "<div class='sheet'></div>";
-echo "<div id='register'>This is where you register</div>";
-echo "<div id='login'>Log in!</div>";
+echo <<<_HDOC
+<div class='sheet'></div>
+<div id='register'>
+	Register
+	<form method="post" action="www.lathamcity.com/blog/index.php">
+		<table>
+		<tr>
+			<td>Userame: <input type="text" name="reg_name" size="32" maxlength="32" /></td>
+		</tr><tr>
+			<td>Password: <input type="password" name="reg_password" size="32" maxlength="32" /></td>
+		</tr><tr>
+			<td>E-mail: <input type="text" name="reg_email" size="32" maxlength="32" /></td>
+		</tr><tr>
+			<td>Full Name: <input type="text" name="reg_fullname" size="32" maxlength="32" /></td>
+		</tr><tr>
+			<td><input type="submit" value="Submit" /></td>
+		</tr>
+		</table>
+	</form>
+</div>
+
+<div id='login'>
+	Login
+	<form method="post" action="www.lathamcity.com/blog/index.php">
+		<table>
+		<tr>
+			<td>Username: <input type="text" name="login_name" size="32" maxlength="32" /></td>
+		</tr><tr>
+			<td>Password: <input type="password" name="login_password" size="32" maxlength="32" /></td>
+		</tr><tr>
+			<td><input type="submit" value="Log in" /></td>
+		</tr>
+		</table>
+	</form>
+</div>
+_HDOC;
 ?>
